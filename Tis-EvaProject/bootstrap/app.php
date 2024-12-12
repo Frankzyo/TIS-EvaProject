9957b425cd9a93c7186f402d7f730f076f1f95d9
Luis Sotillo <luissotillo11@gmail.com>
2024-12-12 11:32:47 -0400

    documentationProvidersApp

--- Tis-EvaProject/bootstrap/app.php
+++ Tis-EvaProject/bootstrap/app.php
@@ -1,3 +1,5 @@
+/* This PHP code snippet is configuring an application using the Illuminate Foundation components.
+Here's a breakdown of what it does: */
 <?php
 
 use Illuminate\Foundation\Application;
--- Tis-EvaProject/bootstrap/providers.php
+++ Tis-EvaProject/bootstrap/providers.php
@@ -1,3 +1,7 @@
+/* This PHP code snippet is defining an array using the `return` statement. The array contains two
+elements, which are class names `App\Providers\AppServiceProvider::class` and
+`App\Providers\AuthServiceProvider::class`. This code is typically used in Laravel framework's
+service provider configuration to register service providers. */
 <?php
 
 return [
